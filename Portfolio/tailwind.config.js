ce2f1a0a81d13be2595e7791cfcf3f790893f768
Jamila Farhana <jamilafarhanaa@gmail.com>
2025-03-09 21:27:15 +0700

    On branch main  Your branch is up to date with 'origin/main'.
     Changes to be committed:
    	modified:   Form/inventori.html
    	new file:   Portfolio/dist/img/gambar.jpg
    	new file:   Portfolio/dist/img/gambar1.png
    	new file:   Portfolio/dist/img/gambar2.png
    	modified:   Portfolio/dist/output.css
    	modified:   Portfolio/index.html
    	modified:   Portfolio/tailwind.config.js

--- Portfolio/tailwind.config.js
+++ Portfolio/tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./index.html"],
   theme: {
-    extend: {},
+    container:{
+      center: true,
+      padding: '16px',
+    },
+    extend: {
+      colors:{
+        primary: '#fb923c',
+        dark: '#052e16'
+      },
+      screens: {
+        '2xl' : '1320px',
+      },
+    },
   },
   plugins: [],
 }
