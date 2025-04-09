e1db15bde6adad7e437062e52d032a5d716ffce5
IndiAriyanti <153523135+IndiAriyanti@users.noreply.github.com>
2025-04-09 20:17:59 +0700

    ini home nya masih gabung about us

--- cemalcemil.com/index.php
+++ cemalcemil.com/index.php
@@ -17,13 +17,13 @@
     <link rel="stylesheet" href="style.css">
 
 </head>
-<body class="d-flex flex-column min-vh-100">
+<body class="d-flex flex-column min-vh-100" style="padding-top: 70px;">
   <!-- navbar -->
   <div class="container-fluid p-0">
-    <nav class="navbar navbar-expand-xl">
+  <nav class="navbar navbar-expand-xl fixed-top">
       <div class="container-fluid">
-        <img src="./img/logo.png" alt="logo" class="img-fluid" style="max-width: 50px;">
-        <a class="navbar-brand text-white fw-bold" href="#">Banana</a>
+        <img src="./img/logonew.png" alt="logo" class="img-fluid" style="max-width: 50px;">
+        <a class="navbar-brand text-white fw-bold" href="#">Cemal Cemil</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" 
                 data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" 
                 aria-expanded="false" aria-label="Toggle navigation">
@@ -32,16 +32,16 @@
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <ul class="navbar-nav me-auto mb-2 mb-lg-0">
             <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#">Home</a>
+              <a class="nav-link active" aria-current="page" href="index.php">Home</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Our Product</a>
+              <a class="nav-link" href="product.php">Our Product</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">About Us</a>
+              <a class="nav-link" href="index.php">About Us</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="#">Cart</a>
+              <a class="nav-link" href="cart.php">Cart</a>
             </li>
             <li class="nav-item">
               <a class="nav-link" href="#">Contact</a>
@@ -56,69 +56,34 @@
   </div>
   <!-- navbar end -->
 
-<!-- first child -->
-
-<div class="bg-light">
-    <h3 class="text-center">Product</h3>
-    <p class="text-center">nanti diisi penjelasan produk Lorem ipsum dolor sit amet consectetur.</p>
-</div>
-
-<!-- first child end -->
-
-<!-- second child -->
-
 <div class="row">
     <div class="col-md-10 mx-auto">
-        <!-- product -->
-         <div class="row">
-            <div class="col-md-4 mb-2">
-                <div class="card">
-                    <img src="./img/1.jpg" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Beli Sekarang</a>
-                        </div>
-                </div>
-            </div>
-            <div class="col-md-4 mb-2">
-                <div class="card">
-                    <img src="./img/2.jpg" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Beli Sekarang</a>
-                        </div>
-                </div>
-            </div>
-            <div class="col-md-4 mb-2">
-                <div class="card">
-                    <img src="./img/OIP.jpeg" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Beli Sekarang</a>
-                        </div>
-                </div>
-            </div>
-            <div class="col-md-4 mb-2">
-                <div class="card">
-                    <img src="./img/2.jpg" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Beli Sekarang</a>
-                        </div>
-                </div>
-            </div>
-            <div class="col-md-4 mb-2">
-                <div class="card">
-                    <img src="./img/2.jpg" class="card-img-top">
-                        <div class="card-body">
-                            <h5 class="card-title">Card title</h5>
-                            <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                            <a href="#" class="btn btn-primary">Beli Sekarang</a>
-                        </div>
+        <!-- Why Cemal Cemil Section -->
+<section class="container my-5">
+  <div class="row align-items-center">
+    <div class="col-md-6">
+      <img src="img/why.png" alt="Kenapa Cemal Cemil" class="img-fluid rounded shadow-sm">
+    </div>
+    <div class="col-md-6">
+      <h4 class="fw-bold text-success">Why Cemal Cemil?</h4>
+      <p class="text-muted">Cemal Cemil menggunakan bahan baku lokal yang berkualitas tinggi dan diproses secara higienis untuk menghasilkan keripik pisang dengan berbagai rasa yang unik dan menggugah selera. Cocok untuk segala usia, baik untuk camilan santai maupun oleh-oleh khas Indonesia.</p>
+    </div>
+  </div>
+</section>
+
+<!-- Who Are We Section -->
+<section class="container my-5">
+  <div class="row align-items-center">
+    <div class="col-md-6 order-md-2">
+      <img src="img/who.png" alt="Tentang Kami" class="img-fluid rounded shadow-sm">
+    </div>
+    <div class="col-md-6 order-md-1">
+      <h4 class="fw-bold text-success">Who Are We</h4>
+      <p class="mb-1 fw-semibold">Bumi Raya</p>
+      <p class="text-muted">Kami berdiri pada Maret 2025 dan berfokus memproduksi keripik pisang, granola, hingga susu pisang dengan aneka rasa. Semua produk kami dibuat dari bahan-bahan pilihan dengan kualitas terbaik demi memastikan rasa lezat, sehat, dan aman untuk dikonsumsi.</p>
+    </div>
+  </div>
+</section>
                 </div>
             </div>
          </div>
@@ -132,8 +97,8 @@
   <div class="container">
     <div class="row gy-4">
       <div class="col-md-3">
-        <h4 class="text-success fw-bold">Banana</h4>
-        <p>Banana - produsen keripik pisang, granola, cookies granola, dan tepung pisang</p>
+        <h4 class="text-success fw-bold">Cemal Cemil</h4>
+        <p>Cemal Cemil - produsen keripik pisang, granola, dan susu pisang</p>
         <div class="d-flex gap-3">
           <a href="#"><i class="fab fa-instagram text-white fs-4"></i></a>
           <a href="#"><i class="fab fa-facebook text-white fs-4"></i></a>
@@ -142,8 +107,8 @@
       <div class="col-md-3">
         <h6 class="fw-bold">Pages</h6>
         <ul class="list-unstyled">
-          <li><a href="#" class="footer-link">Our Product</a></li>
-          <li><a href="#" class="footer-link">About us</a></li>
+          <li><a href="product.php" class="footer-link">Our Product</a></li>
+          <li><a href="index.php" class="footer-link">About us</a></li>
           <li><a href="#" class="footer-link">Cart</a></li>
           <li><a href="#" class="footer-link">Contact</a></li>
         </ul>
@@ -152,7 +117,7 @@
       <div class="col-md-3">
         <h6 class="fw-bold">Our info</h6>
         <p><span class="text-warning">6288888888</span><br>
-          <a href="mailto:bumiraya@banana.com" class="footer-link">bumiraya@banana.com</a><br>
+          <a href="mailto:bumiraya@CemalCemil.com" class="footer-link">bumiraya@CemalCemil.com</a><br>
           Jl. Rungkut Madya, Gn. Anyar, Kec. Gn. Anyar,<br>
           Surabaya, Jawa Timur 60294, Indonesia
         </p>
