48dd9f9115082419c631ab3074f193849b082cf9
Jamila Farhana <jamilafarhanaa@gmail.com>
2025-06-02 02:07:05 +0700

    update route

--- routes/web.php
+++ routes/web.php
@@ -16,17 +16,9 @@ Route::get('/product', function () {
     return view('product');
 });
 
-// Route::get('/login', function () {
-//     return view('login');
-// });
-
 Route::get('/login', [LoginController::class, 'showLoginForm'])->name('login');
 Route::post('/login', [LoginController::class, 'login'])->name('login.submit');
 Route::post('/logout', [LoginController::class, 'logout'])->name('logout');
-
-// Route::get('/signup', function () {
-//     return view('signup');
-// });
 
 Route::get('/register', [RegisterController::class, 'showRegisForm'])->name('register.form');
 Route::post('/register', [RegisterController::class, 'register'])->name('register.submit');
