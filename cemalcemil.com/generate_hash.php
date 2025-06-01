a28515cae3b71deb65477e7c203b15f23680e746
Jamila Farhana <145261092+mila155@users.noreply.github.com>
2025-06-02 02:49:09 +0700

    Delete cemalcemil.com directory

--- cemalcemil.com/generate_hash.php
+++
@@ -1,5 +0,0 @@
-<?php
-$password = 'superadmin123'; // ganti dengan password yang kamu mau
-$hash = password_hash($password, PASSWORD_DEFAULT);
-echo "Hash: " . $hash;
-?>
--- cemalcemil.com/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-session_unset();  // Hapus semua data session
-session_destroy(); // Hapus session sepenuhnya
-
-header("Location: index.php"); // Redirect ke halaman depan
-exit;
-?>
